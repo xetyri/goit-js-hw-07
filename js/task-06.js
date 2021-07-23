02348ddd2a4b56350d89ac2efcb5e62fa20f3cfc
Roman Leshchenko <xetyri2014@gmail.com>
2021-07-23 14:48:34 +0300

    Правки

--- js/task-06.js
+++ js/task-06.js
@@ -4,14 +4,10 @@ const getLength = Number(inp.getAttribute("data-length"));
 inp.addEventListener("blur", gett)
 
 function gett() {
+        inp.classList.add('invalid');
+        inp.classList.remove('valid');
     if (inp.value.length === getLength) {
         inp.classList.add("valid");
-        inp.classList.remove('invalid');
-    } else if(inp.value.length > getLength){
-        inp.classList.add("invalid");
-        inp.classList.remove('valid');
-    } else {
-        inp.classList.remove('invalid');
-        inp.classList.remove('valid');
-    }
+        inp.classList.remove('invalid');  
+    } 
 }
